81e2a29ab468e973985397c9ce52f928d15bada5
Peter Beerli <beerli@me.com>
2022-01-21 16:11:51 -0500

    starting to upload the parts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Lineage label switching: Datasets, mathematical supplement, links to software used 
-Supplement to Population divergence time estimation using individual lineage label switching, contains raw data filesMath explanations and 
+# Lineage label switching: 
+## Datasets, mathematical supplement, links to software used 
+Supplement to Population divergence time estimation using individual lineage label switching, contains all the scripts to generate the simulated data, math explanations, and the real data of the examples. 
---
+++ simulations/modified_ms/manyloci.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# using as argument:  ms 20 5 -t 10 -r 0 1000 -I 2 20 10 -T -M -q 1000 -ej 1.0  1 2 > tree
+import sys
+import os
+
+default = "ms 40 5 -t 10 -r 0 500 -I 2 20 20 -T -M -q 500 -ej 1.0  1 2 "
+if len(sys.argv)<2:
+    msarg = default.split()[:]
+else:
+    msarg = sys.argv[1:]
+#print "#", len(sys.argv)
+#print "#", sys.argv
+#print "#", msarg
+
+loci=long(msarg[2])
+index = msarg.index('-I')
+#print index,msarg[index],msarg[index+1]
+numpop = long(msarg[index+1])
+popstring = " ".join(msarg[index+2:index+numpop+2])
+index = msarg.index('-q')
+sites = msarg[index+1]
+ms = " ".join(filter(None, msarg))
+#print "@",ms,"@"
+os.system(ms+" | grep \"^(\" > gugus")
+g = open("gugus","rU")
+print "#SN"
+print "#-1"
+print "#"+str(numpop)
+print "#"+popstring 
+print "#"+str(loci)+" "+sites+" 2.000000"
+rl = range(loci)
+
+for i in rl:
+    print "# rate among sites for locus 0 (1.000000)"
+    print "#="
+loc = 0    
+for gi in g:
+    loc += 1
+    print "#$Locus "+str(loc)
+    print "#$1.0"
+    print gi,
+
